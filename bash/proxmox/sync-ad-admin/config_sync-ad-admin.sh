6361c8bf4122530955ddcaaea770c6fdf82552cd
devenzopro <dev-enzo@proton.me>
2025-07-15 15:45:31 +0200

    Create config_sync-ad-admin.sh

---
+++ bash/proxmox/sync-ad-admin/config_sync-ad-admin.sh
@@ -0,0 +1,7 @@
+# config.sh
+AD_SERVER="192.168.1.10"
+BIND_DN="admin@domaine.local"
+BIND_PW_FILE="/root/.ldap_pass"
+GROUP_DN="CN=GG_ADMIN-PVE,OU=Groups,DC=domain,DC=local"
+REALM="ad-internet"
+PROXMOX_GROUP="GG_ADMIN-PVE_CRV-AD-INTERNET"
