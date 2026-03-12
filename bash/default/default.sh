b4194be1fc8546850760e4066f0d6d772da6a3be
devenzopro <dev-enzo@proton.me>
2026-03-12 15:12:28 +0100

    Update default.sh

--- bash/default/default.sh
+++ bash/default/default.sh
@@ -27,6 +27,13 @@ run_step() {
     if [[ "$choix" =~ ^[yY]$ ]]; then
         echo -e "${VERT}Exécution de ${NOM_PARTIE}...${NC}"
         curl -sSL "$URL_PARTIE" | sudo bash
+        if command -v sudo >/dev/null 2>&1; then
+            # Si sudo est installé
+            sudo ufw status verbose
+        else
+            # Si sudo n'est pas installé (on est probablement déjà en root)
+            ufw status verbose
+        fi
     else
         echo -e "${ROUGE}Étape ${NOM_PARTIE} sautée.${NC}"
     fi
